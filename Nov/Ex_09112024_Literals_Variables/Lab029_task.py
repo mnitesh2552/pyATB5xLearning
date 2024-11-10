88337d17390f90fc450407b55f09b598fa7dd728
Nitesh KumarPYlab001 <mnitesh.0055@gmail.com>
2024-11-10 21:29:25 +0530

    9th November part 2

---
+++ Nov/Ex_09112024_Literals_Variables/Lab029_task.py
@@ -0,0 +1,12 @@
+# Take 3 input from user and perform sum, sub, mul, div
+num1 = int(input("Enter the num 1"))
+num2 = int(input("Enter the num 2"))
+num3 = int(input("Enter the num 3"))
+Sum = num1 + num2 + num3
+Sub = num1 - num2 - num3
+Mul = num1 * num2 * num3
+Div = num1 / num2 / num3
+print("Sum is -> ", Sum)
+print("Sub is -> ", Sub)
+print("Mul is -> ", Mul)
+print("Div is -> ", Div)
